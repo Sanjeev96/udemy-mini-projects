e2eca5566b51f70f43ea7722ac43c1971ba71c50
Sanjeev96 <sanjeevdhillon12@hotmail.co.uk>
2019-03-11 21:44:17 +0000

    observables: subscribe, manually creating   observable and unsubscribe

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,23 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, interval, Observer } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, interval, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  public numberObsSub: Subscription;
+  public customObsSub: Subscription;
+
   constructor() {}
 
   ngOnInit() {
-    // interval is a utility function used in rxj
+    // interval; is; a; utility; function used in rxj;
     const myNum = interval(1000);
-    myNum.subscribe(number => {
-      console.log('this is the number', number );
+
+    this.numberObsSub = myNum.subscribe(number => {
+      console.log('this is the number', number);
     });
 
-    // const myObservable = create((observer: Observer) => {
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        observer.next('third package');
+      }, 6000);
+    });
 
-    // });
+    this.customObsSub = myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      () => {
+        console.log('completed');
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    // used to stop memeory leak - subscription running when not needed are inefficent
+    this.numberObsSub.unsubscribe();
+    this.customObsSub.unsubscribe();
   }
 }
