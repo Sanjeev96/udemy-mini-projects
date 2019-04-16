5dc5b13d2600cb714620f47858b47e38b3a36836
sanjeeve1996 <sanjeev.dhillon@esure.com>
2019-04-16 15:06:17 +0100

    setting validity via css for form fields

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Form, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   @ViewChild('Registerform') registerform: NgForm;
