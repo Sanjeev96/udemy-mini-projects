cf8a0c6aa42b085d2ab618bda7e38a3f84e44e46
sanjeeve1996 <sanjeev.dhillon@esure.com>
2019-04-23 13:27:37 +0100

    folder structure update

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,13 +9,50 @@ import { Form, NgForm } from '@angular/forms';
 export class AppComponent implements OnInit {
   @ViewChild('Registerform') registerform: NgForm;
   // @ViewChild('_pet') pet: ElementRef;
-  defaultQ = 'pet';
+  defaultQ = 'teacher';
+  defaultGender = 'Male';
   public answer = '';
+  genders = ['Male', 'Female', 'Other'];
+  user = {
+    username: '',
+    email: '',
+    secretQ: '',
+    secretA: '',
+    gender: ''
+  };
+
+  public submitted = false;
 
   ngOnInit() {}
 
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = [
+      'Superuser',
+      'user123',
+      'Dragonslay3er',
+      'c3pO',
+      '123abc'
+    ];
+    const randomUsername =
+      suggestedName[Math.floor(Math.random() * suggestedName.length)];
+    console.log(this.suggestUserName);
+    // this.registerform.setValue({ // this approach means you must set a value for each item in the form...NOT THE BEST APPROACH
+    //   UserData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   UserSecret: {
+    //     secrect: 'pet',
+    //     qA: '',
+    //     gender: 'Male'
+    //   }
+    // });
+    this.registerform.form.patchValue({
+      // 'patchValue overrides parts of the form'
+      UserData: {
+        username: randomUsername
+      }
+    });
   }
 
   // EXAMPLE 1 PASSING FORM AS A PARAMATER
@@ -25,6 +62,14 @@ export class AppComponent implements OnInit {
 
   //EXAMPLE 2 USING VIEWCHILD
   onSubmit() {
-    console.log(this.registerform);
+    this.submitted = true;
+    this.user.username = this.registerform.value.UserData.username;
+    this.user.email = this.registerform.value.UserData.email;
+    this.user.secretQ = this.registerform.value.UserSecret.secretQ;
+    this.user.secretA = this.registerform.value.UserSecret.secretA;
+    this.user.gender = this.registerform.value.gender;
+
+    this.registerform.resetForm();
+
   }
 }
